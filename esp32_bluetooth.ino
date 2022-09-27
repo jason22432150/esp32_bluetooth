7e1bb6f5c32b768adc2c60b7c352758bc1a07221
國楨 <nmsl22432150@gmail.com>
2022-09-27 20:57:18 +0800

    save

--- esp32_bluetooth.ino
+++ esp32_bluetooth.ino
@@ -17,51 +17,41 @@ String now_mode = "stop";
 String mode_data;
 String BT_String = "";
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32_BT"); //藍芽裝置名稱
+  SerialBT.begin("ESP32_BT");  //藍芽裝置名稱
   Serial.println("The device started, now you can pair it with bluetooth!");
-  pixels1.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels2.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  for (int i = 0; i < NUMPIXELS; i++)
-  {
+  pixels1.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels2.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+  for (int i = 0; i < NUMPIXELS; i++) {
     pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
-    pixels1.show(); // Send the updated pixel colors to the hardware.
+    pixels1.show();  // Send the updated pixel colors to the hardware.
     pixels2.setPixelColor(i, pixels2.Color(0, 0, 0));
-    pixels2.show(); // Send the updated pixel colors to the hardware.
+    pixels2.show();  // Send the updated pixel colors to the hardware.
   }
 }
 
-void loop()
-{
-  if (Serial.available())
-  {
+void loop() {
+  if (Serial.available()) {
     // 電腦端接受資料發送到藍牙
     SerialBT.write(Serial.read());
   }
-  if (SerialBT.available())
-  {
+  if (SerialBT.available()) {
     //藍牙端接受資料發送到電腦
     char BT_Input_String = SerialBT.read();
-    if (String(BT_Input_String) != "")
-    {
-      if (String(BT_Input_String) == "%")
-      {
-        if (BT_String.indexOf("stop") != -1)
-        {
+    if (String(BT_Input_String) != "") {
+      if (String(BT_Input_String) == "%") {
+        if (BT_String.indexOf("stop") != -1) {
           // stop
+          mate_stop();
           now_mode = "stop";
         }
-        if (BT_String.indexOf("mode") != -1)
-        {
+        if (BT_String.indexOf("mode") != -1) {
           // mode123
           now_mode = "mode";
           mode_data = String(BT_String.substring(4, BT_String.length()));
           mate_Mode(String(BT_String.substring(4, BT_String.length())));
-        }
-        else if (BT_String.indexOf("rgb") != -1)
-        {
+        } else if (BT_String.indexOf("rgb") != -1) {
           String R, G, B;
           // rgb(255,255,255)
           // 0123456789
@@ -72,77 +62,64 @@ void loop()
         }
         print_test("BT_String", BT_String);
         BT_String = "";
-      }
-      else
-      {
+      } else {
         BT_String += BT_Input_String;
       }
     }
   }
   delay(20);
-  if (now_mode == "mode")
-  {
+  if (now_mode == "mode") {
     mate_Mode(String(mode_data));
   }
 }
 
-void mate_Mode(String mode)
-{
+void mate_Mode(String mode) {
   print_test("mate_Mode", mode);
-  switch (mode)
-  {
-  case 1:
-    mode01();
+  switch (mode.toInt()) {
+    case 1:
+      mode01();
 
-  default:
-    break;
+    default:
+      break;
   }
 }
 
-void stop()
-{
-  for (int i = 0; i < NUMPIXELS; i++)
-  {
+void mate_stop() {
+  for (int i = 0; i < NUMPIXELS; i++) {
     pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
-    pixels1.show(); // Send the updated pixel colors to the hardware.
+    pixels1.show();  // Send the updated pixel colors to the hardware.
     pixels2.setPixelColor(i, pixels2.Color(0, 0, 0));
-    pixels2.show(); // Send the updated pixel colors to the hardware.
+    pixels2.show();  // Send the updated pixel colors to the hardware.
   }
   now_mode = "stop";
   print_test("stop", "STOP");
 }
 
-void mate_RGB(String R, String G, String B)
-{
+void mate_RGB(String R, String G, String B) {
   print_test("mate_RGB R", String(R));
   print_test("mate_RGB G", String(G));
   print_test("mate_RGB B", String(B));
 }
 
-String make_string_three(String string)
-{
-  while (string.length() != 3)
-  {
+String make_string_three(String string) {
+  while (string.length() != 3) {
     string = "0" + string;
   };
   return (string);
 }
 
-void print_test(String typeTxt, String inputTxt)
-{
+void print_test(String typeTxt, String inputTxt) {
   Serial.print(typeTxt);
   Serial.print(": ");
   Serial.println(inputTxt);
 }
 
 // 以下為自訂mode
-void mode01()
-{
-  for (int i = 0; i < NUMPIXELS; i++)
-  { //(R, G,  B )
+void mode01() {
+  for (int i = 0; i < NUMPIXELS; i++) {  //(R, G,  B )
     pixels1.setPixelColor(i, pixels1.Color(0, 0, 255));
-    pixels1.show(); // Send the updated pixel colors to the hardware.
+    pixels1.show();  // Send the updated pixel colors to the hardware.
     pixels2.setPixelColor(i, pixels2.Color(200, 0, 255));
-    pixels2.show(); // Send the updated pixel colors to the hardware.
+    pixels2.show();  // Send the updated pixel colors to the hardware.
   }
 }
